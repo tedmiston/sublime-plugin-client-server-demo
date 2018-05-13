809d321f6ef188db6ee1bb059b3eff5298be14ee
Taylor D. Edmiston <tedmiston@gmail.com>
2018-05-13 17:53:50 -0400

    Tweak whitespace

--- server/server.py
+++ server/server.py
@@ -5,24 +5,30 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route('/bash', methods=['POST'])
 def bash():
     command = request.json['command']
+
     proc = subprocess.run(command, stdout=subprocess.PIPE, shell=True,
                           universal_newlines=True)
     output = proc.stdout
+
     return jsonify({
         'command': command,
         'output': output,
     })
 
+
 @app.route('/upper', methods=['POST'])
 def upper():
     text = request.json['text']
+
     return jsonify({
         'input': text,
         'output': text.upper(),
     })
+
 
 @app.route('/version', methods=['GET'])
 def version():
