34f0e7830c07d622d69ddab49cbbc33603a5bc4b
Taylor D. Edmiston <tedmiston@gmail.com>
2018-05-13 17:34:00 -0400

    Sort routes

--- server/server.py
+++ server/server.py
@@ -4,16 +4,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/version', methods=['GET'])
-def version():
-    return jsonify({
-        'version': sys.version,
-    })
-
 @app.route('/upper', methods=['POST'])
 def upper():
     text = request.json['text']
     return jsonify({
         'input': text,
         'output': text.upper(),
+    })
+
+@app.route('/version', methods=['GET'])
+def version():
+    return jsonify({
+        'version': sys.version,
     })
